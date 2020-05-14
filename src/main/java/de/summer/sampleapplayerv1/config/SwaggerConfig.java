6e4090d086504b93628ffc52a534739fb5fae374
venky424 <venkateshdwarapureddi0789@gmail.com>
2020-05-14 12:04:10 +0200

    Intraday Updates as of 14052020

--- src/main/java/de/summer/sampleapplayerv1/config/SwaggerConfig.java
+++ src/main/java/de/summer/sampleapplayerv1/config/SwaggerConfig.java
@@ -1,7 +1,8 @@
 package de.summer.sampleapplayerv1.config;
 
-import io.swagger.v3.oas.annotations.info.Info;
+
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
